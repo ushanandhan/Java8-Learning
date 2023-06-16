d81f5e6f646d0ccbf6af69fc8a0fa8c01ec8096e
Ushanandhan <ariyaushan@gmail.com>
2023-06-16 10:28:51 +0530

    Reflection related Test with field by name is committed.

--- Learning/src/test/java/com/reflection/ReflectionTest.java
+++ Learning/src/test/java/com/reflection/ReflectionTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.List;
 
 public class ReflectionTest {
@@ -18,16 +19,28 @@ public class ReflectionTest {
 
     @Test
     public void test() throws IllegalAccessException {
+        String[] columns = {"name","gpa"};
         Student student = students.get(0);
-        printFields(student);
+        printFields(student,columns);
     }
 
-    public static void printFields(Object obj) throws IllegalAccessException {
+    public static void printFields(Object obj,String[] columns) {
         Class<?> clazz = obj.getClass();
-        Field[] fields = clazz.getDeclaredFields();
-        for (Field field : fields) {
-            field.setAccessible(true);
-            System.out.println(field.getName() + " = " + field.get(obj));
-        }
+
+        Arrays.stream(columns).forEach(column -> {
+            try {
+                Field field = clazz.getDeclaredField(column);
+                field.setAccessible(true);
+                System.out.println(field.getName() + " = " + field.get(obj));
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+//        Field[] fields = clazz.getDeclaredFields();
+//        for (Field field : fields) {
+//            field.setAccessible(true);
+//            System.out.println(field.getName() + " = " + field.get(obj));
+//        }
     }
 }
